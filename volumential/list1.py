9947eb663d9250d6b1f7038b8f063ed87d2954e2
xywei <wxy0516@gmail.com>
2019-05-26 11:31:54 -0500

    Doc list1 eval

--- volumential/list1.py
+++ volumential/list1.py
@@ -20,6 +20,13 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
+__doc__ = """
+.. autoclass:: NearFieldEvalBase
+   :members:
+.. autoclass:: NearFieldFromCSR
+   :members:
+"""
+
 import numpy as np
 import loopy
 
