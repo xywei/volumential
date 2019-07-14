0e8cab30d55025b1de8b1024d35983ad6a34daa5
xywei <wxy0516@gmail.com>
2019-07-14 17:22:19 -0500

    Add discrete Legendre transform

--- volumential/tools.py
+++ volumential/tools.py
@@ -22,10 +22,15 @@ THE SOFTWARE.
 
 import numpy as np
 import loopy as lp
+import pyopencl as cl
 import pymbolic as pmbl
 from pymbolic.primitives import Variable as VariableType
 from pymbolic.primitives import Expression as ExpressionType
 from sumpy.tools import KernelCacheWrapper
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 # {{{ clean files
 
@@ -241,3 +246,165 @@ def import_code(code, name, add_to_sys_modules=True):
     return module
 
 # }}} End import code
+
+# {{{ discrete Legendre transform
+
+
+class DiscreteLegendreTransform(KernelCacheWrapper):
+    """
+    Transform from nodal values to Legendre polynomial coefficients
+    for all cells (leaf boxes of a boxtree Tree object)
+    """
+
+    def __init__(self, dim, degree):
+        """
+        :arg dim
+        :arg degree Number of nodes in each axis direction.
+        """
+        assert dim > 0
+        self.dim = dim
+        assert degree > 0
+        self.degree = degree
+
+        # Template interval
+        self.template_interval = [-1., 1.]
+        self.template_interval_extent = 2.
+        self.template_interval_center = 0.
+
+        self.leg_tplt_x, self.leg_tplt_w = \
+                np.polynomial.legendre.leggauss(degree)
+
+        if self.dim == 1:
+            self.V = np.polynomial.legendre.legvander(
+                    self.leg_tplt_x, self.degree - 1)
+            self.W = self.leg_tplt_w.reshape(-1)
+
+        elif self.dim == 2:
+            x, y = np.meshgrid(self.leg_tplt_x, self.leg_tplt_x)
+            self.V = np.polynomial.legendre.legvander2d(
+                    x.reshape(-1), y.reshape(-1),
+                    [self.degree - 1] * self.dim)
+            self.W = (self.leg_tplt_w[:, None]
+                    * self.leg_tplt_w[None, :]).reshape(-1)
+
+        elif self.dim == 3:
+            x, y, z = np.meshgrid(self.leg_tplt_x, self.leg_tplt_x, self.leg_tplt_x)
+            self.V = np.polynomial.legendre.legvander3d(
+                    x.reshape(-1), y.reshape(-1), z.reshape(-1),
+                    [self.degree - 1] * self.dim)
+            self.W = (self.leg_tplt_w[:, None, None]
+                    * self.leg_tplt_w[None, :, None]
+                    * self.leg_tplt_w[None, None, :]).reshape(-1)
+
+        else:
+            raise NotImplementedError("Dimension %d is not supported" % self.dim)
+
+        # Vandermonde matrix: each column corresponds to one basis function
+        assert self.V.shape == (self.degree**self.dim, self.degree**self.dim)
+        assert self.W.shape == (self.degree**self.dim,)
+
+        # Normalizers
+        self.I = np.ascontiguousarray(np.diag(self.V.T * self.W @ self.V))
+        assert self.I.shape == (self.degree**self.dim,)
+
+        # Fix strides for loopy
+        self.V = np.ascontiguousarray(self.V)
+
+        # Check orthogonality
+        ortho_resid = np.linalg.norm(
+                self.V.T * self.W @ self.V - np.diag(self.I))
+        if ortho_resid > 1e-13:
+            logger.warn("Legendre polynomials' orthogonality residual = %f"
+                    % ortho_resid)
+
+        self.name = "DiscreteLegendreTransform"
+
+    def get_cache_key(self):
+        return (
+            type(self).__name__,
+            str(self.dim) + "D",
+            "degree=%d" % self.degree
+        )
+
+    def get_kernel(self, **kwargs):
+
+        loopy_knl = lp.make_kernel(  # NOQA
+                [
+                    "{ [ bid ] : 0 <= bid < n_boxes }",
+                    "{ [ mid ] : 0 <= mid < n_box_nodes }",
+                    "{ [ nid ] : 0 <= nid < n_box_nodes }"
+                    ],
+                [
+                    """
+                for bid
+                    <> box_id       = boxes[bid]
+                    <> box_node_beg = box_node_starts[box_id]
+
+                    <> box_level    = box_levels[box_id]
+                    <> box_extent   = root_extent * (1.0 / (2**box_level))
+
+                    for mid
+                        <> mode_id = box_node_beg + mid
+                        result[mode_id] = sum(
+                                              nid,
+                                              func[box_node_beg + nid] * weight[nid] * vandermonde[nid, mid]
+                                             ) / normalizer[mid]
+                    end
+                end
+                """
+                ],
+                [
+                    lp.ValueArg("n_box_nodes, n_boxes", np.int32),
+                    lp.ValueArg("root_extent", np.float64),
+                    lp.GlobalArg("weight, normalizer", np.float64, "n_box_nodes"),
+                    lp.GlobalArg("vandermonde", np.float64, "n_box_nodes, n_box_nodes"),
+                    lp.GlobalArg("func", np.float64, "n_box_nodes * n_boxes"),
+                    "...",
+                    ],
+                name="discrete_legendre_transform",
+                lang_version=(2018, 2),
+                )
+
+        loopy_knl = lp.set_options(loopy_knl, write_cl=False)
+        loopy_knl = lp.set_options(loopy_knl, return_dict=True)
+
+        return loopy_knl
+
+    def get_optimized_kernel(self, ncpus=None, **kwargs):
+        knl = self.get_kernel(**kwargs)
+        if ncpus is None:
+            import os
+            ncpus = os.cpu_count()
+        knl = lp.split_iname(
+            knl,
+            split_iname="bid",
+            inner_length=ncpus,
+            inner_tag="l.0")
+        return knl
+
+    def __call__(self, queue, traversal, nodal_vals, **kwargs):
+        """
+        :arg traversal
+        :arg nodal_vals
+        """
+
+        knl = self.get_cached_optimized_kernel()
+
+        evt, res = knl(
+            queue,
+            boxes=traversal.target_boxes,
+            box_node_starts=traversal.tree.box_target_starts,
+            box_levels=traversal.tree.box_levels,
+            root_extent=traversal.tree.root_extent,
+            func=nodal_vals,
+            weight=cl.array.to_device(queue, self.W),
+            vandermonde=cl.array.to_device(queue, self.V),
+            normalizer=cl.array.to_device(queue, self.I),
+            n_box_nodes=self.degree**self.dim,
+            n_boxes=traversal.target_boxes.shape[0],
+            result=cl.array.zeros_like(nodal_vals),
+        )
+
+        return res["result"]
+
+# }}} End discrete Legendre transform
