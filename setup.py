e37f7e0700375b1ef68154bd577d8bb357df4c0b
xywei <wxy0516@gmail.com>
2019-10-24 12:02:17 -0500

    Setup git revision on setup.py run

--- setup.py
+++ setup.py
@@ -23,15 +23,66 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
-from setuptools import setup, find_packages
+import os
+from setuptools import find_packages, setup
+
+
+# {{{ capture git revision at install time
+
+# authoritative version in pytools/__init__.py
+def find_git_revision(tree_root):
+    # Keep this routine self-contained so that it can be copy-pasted into
+    # setup.py.
+
+    from os.path import join, exists, abspath
+    tree_root = abspath(tree_root)
+
+    if not exists(join(tree_root, ".git")):
+        return None
+
+    from subprocess import Popen, PIPE, STDOUT
+    p = Popen(["git", "rev-parse", "HEAD"], shell=False,
+              stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True,
+              cwd=tree_root)
+    (git_rev, _) = p.communicate()
+
+    import sys
+    if sys.version_info >= (3,):
+        git_rev = git_rev.decode()
+
+    git_rev = git_rev.rstrip()
+
+    retcode = p.returncode
+    assert retcode is not None
+    if retcode != 0:
+        from warnings import warn
+        warn("unable to find git revision")
+        return None
+
+    return git_rev
+
+
+def write_git_revision(package_name):
+    from os.path import dirname, join
+    dn = dirname(__file__)
+    git_rev = find_git_revision(dn)
+
+    with open(join(dn, package_name, "_git_rev.py"), "w") as outf:
+        outf.write("GIT_REVISION = %s\n" % repr(git_rev))
+
+
+# }}}
 
 
 def main():
-    from setuptools import setup
-
     version_dict = {}
     init_filename = "volumential/version.py"
-    exec(compile(open(init_filename, "r").read(), init_filename, "exec"), version_dict)
+    os.environ["AKPYTHON_EXEC_FROM_WITHIN_WITHIN_SETUP_PY"] = "1"
+    exec(compile(
+        open(init_filename, "r").read(), init_filename, "exec"),
+        version_dict)
+
+    write_git_revision("volumential")
 
     setup(
         name="volumential",
