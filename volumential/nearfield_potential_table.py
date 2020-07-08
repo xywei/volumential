b3660c31fcd8e677c564a220df5831c22bcaf1c6
xywei <wxy0516@gmail.com>
2020-07-08 14:52:00 -0500

    Refactor to make pylint happy

--- volumential/nearfield_potential_table.py
+++ volumential/nearfield_potential_table.py
@@ -459,7 +459,9 @@ class NearFieldInteractionTable(object):
 
         import scipy.special as sps
 
-        cheby_nodes, _, cheby_weights = sps.chebyt(cheb_order).weights.T
+        # quad_info contains: roots, weights, equiv_weights
+        quad_info = sps.chebyt(cheb_order).weights.T
+        cheby_nodes, cheby_weights = (quad_info[0, :], quad_info[2, :])
         window = [0, 1]
 
         cheby_nodes = cheby_nodes * (window[1] - window[0]) / 2 + np.mean(window)
