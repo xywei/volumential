399f9fde643e7a95f1b753fd9f22becfed2ed438
xywei <wxy0516@gmail.com>
2020-10-26 22:43:01 -0500

    Automate docker image fixes

---
+++ docker/to_loopyy.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+cd /home/firedrake/firedrake/src/
+mv loo-py/ loopyy/
+
+# under loopyy
+cd loopyy/
+find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/loopy/loopyy/g'
+find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/loo.py/loo.pyy/g'
+mv loopy loopyy
+mv bin/loopy bin/loopyy
+cd ..
+
+# under meshmode
+cd meshmode/
+find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/loopy/loopyy/g'
+find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/loo.py/loo.pyy/g'
+cd ..
+
+# under pytential
+cd pytential/
+find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/loopy/loopyy/g'
+find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/loo.py/loo.pyy/g'
+find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/from __future__.*//g'  # to use py3.6
+cd ..
+
+# under volumential
+cd volumential/
+find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/loopy/loopyy/g'
+find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/loo.py/loo.pyy/g'
+find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/from __future__.*//g'  # to use py3.6
+cd ..
+
+rm /home/firedrake/firedrake/lib/python3.6/site-packages/loo.py.egg-link
+pip install -e ./loopyy
+pip install -e ./loopy
